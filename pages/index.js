862d26813b2683791649e95befacf60f52801834
tuliobgn <tuliobegena@gmail.com>
2022-01-26 18:01:27 -0300

    Day 2

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import react from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json';
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -47,24 +20,25 @@ function Titulo(props) {
 }
 
 //Componente React
-function HomePage() {
-    // JSX
-    return (
-         <div>
-             <GlobalStyle />
-             <Titulo tag="h2">Hello, AlChemist!</Titulo>
-             <h2>Discord - Alura Matrix</h2>
-         </div>
-     )
- }
+// function HomePage() {
+//     // JSX
+//     return (
+//          <div>
+//              <GlobalStyle />
+//              <Titulo tag="h2">Hello, AlChemist!</Titulo>
+//              <h2>Discord - Alura Matrix</h2>
+//          </div>
+//      )
+//  }
 //export default HomePage
 
 export default function PaginaInicial() {
-  const username = 'tuliobgn';
+    //const username = 'tuliobgn';
+    const [username, setUsername] = react.useState('tuliobgn');
+    const roteamento = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -92,17 +66,34 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function (infosDoEvento) {
+                infosDoEvento.preventDefault();
+                console.log('Form submetido')
+                roteamento.push('/chat');
+                //window.location.href = '/chat';
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
             }}
           >
-            <Titulo tag="h2">Hello, AlChemist!</Titulo>
+            <Titulo tag="h2">Hello, Chemist!</Titulo>
             <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
               {appConfig.name}
             </Text>
 
-            <TextField
+            <input
+            type="text"
+            value={username}
+            onChange={function handler(event) {
+                console.log('usuariodigitou', event.target.value);
+                //Onde está o valor?
+                const valor = event.target.value;
+                //Trocar o valor da variável através do React
+                setUsername(valor);
+            }}
+            />
+            {/* <TextField
               fullWidth
               textFieldColors={{
                 neutral: {
@@ -112,7 +103,7 @@ export default function PaginaInicial() {
                   backgroundColor: appConfig.theme.colors.neutrals[800],
                 },
               }}
-            />
+            /> */}
             <Button
               type='submit'
               label='Enter'
